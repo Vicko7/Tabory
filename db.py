1ba8fdc0fdd0801228593aa5472cde6fce115fe6
Vicko <matvipro@gmail.com>
2019-05-11 18:02:53 +0200

    po dopsani kodu s Danem

---
+++ db.py
@@ -0,0 +1,16 @@
+import psycopg2
+import psycopg2.extras
+import os
+from flask import g
+
+def get_db():
+    """ Spojeni s dtb. """
+
+    if not hasattr(g, 'db') or g.db.closed == 1:
+		# https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-python
+        database_url = os.environ["DATABASE_URL"]
+    #database_url = os.environ["DATABASE_URL"]
+        con = psycopg2.connect(database_url, sslmode='require', cursor_factory=psycopg2.extras.NamedTupleCursor)
+        g.db = con
+    return g.db
+
--- main.py
+++ main.py
@@ -1,14 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, g
+import db
 
 app= Flask("Tabory")
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'db'):
+        # Bezpečně ukončí spojení s naší databází
+        g.db.close()
 
 @app.route('/')
 def index():
     return render_template ("index.html")
 
-@app.route('/tabory')
+@app.route('/tabory', methods=["GET"])
 def tabory():
     return render_template ("tabory.html")
+
+@app.route('/tabory', methods=["POST"])
+def tabory_hledani():
+    print(request.form)
+
+    kraje_sql = ["1=1"]
+    if requests.form.get("kraj_karlovarsky") == 1:
+        kraje_sql.append("kraj_karlovarsky = 1")
+    if requests.form.get("kraj_ustecky") == 1:
+        kraje_sql.append("kraj_ustecky = 1")
+    kraje_sql_where = " OR ".join(kraje_sql)
+
+    conn = db.get_db()
+    sql = """SELECT * FROM camp WHERE  (""" + kraje_sql_where + ")"
+    print(sql)
+    cur = conn.cursor()
+    cur.execute(sql)
+    data = cur.fetchall()
+    print(data)
+    return render_template ("tabory_vysledky.html", tabory_z_db = data)
+
+@app.route('/tabor/<id_taboru>')
+def tabor_detail(id_taboru):
+
+    conn = db.get_db()
+    sql = """SELECT * FROM camp WHERE camp_id = %s"""
+    cur = conn.cursor()
+    cur.execute(sql, (id_taboru, ))
+    data = cur.fetchall()
+
+    return render_template("tabor_detail.html", tabor_z_db = data)
 
 @app.route('/dobrovolnictvi')
 def dobrovolnictvi():
