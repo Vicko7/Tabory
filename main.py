d602a1724f6451b714ad8ac6d0df7545d0567ff6
Vicko <matvipro@gmail.com>
2019-05-11 14:01:28 +0200

    oprava main.py mezry

--- main.py
+++ main.py
@@ -3,31 +3,31 @@ from flask import Flask, render_template
 app= Flask("Tabory")
 
 @app.route('/')
-def index ():
+def index():
     return render_template ("index.html")
 
 @app.route('/tabory')
-def tabory ():
+def tabory():
     return render_template ("tabory.html")
 
-@app.route('/dobrovolnicti')
-def dobrovolnictvi ():
+@app.route('/dobrovolnictvi')
+def dobrovolnictvi():
     return render_template ("dobrovolnictvi.html")
 
 @app.route('/prace')
-def prace ():
+def prace():
     return render_template ("prace.html")
 
 @app.route('/onas')
-def onas ():
+def onas():
     return render_template ("onas.html")
 
 @app.route('/registrace')
-def registrace ():
+def registrace():
     return render_template ("registrace.html")
     
 @app.route('/prihlaseni')
-def prihlaseni ():
+def prihlaseni():
     return render_template ("prihlaseni.html")
 
 
