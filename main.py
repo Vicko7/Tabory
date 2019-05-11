e8347a536b92030ccc90be031145a3beb13c5afe
Vicko <matvipro@gmail.com>
2019-05-11 11:56:07 +0200

    Pridanie do main.py dalsie stranky

--- main.py
+++ main.py
@@ -5,3 +5,30 @@ app= Flask("Tabory")
 @app.route('/')
 def index ():
     return render_template ("index.html")
+
+    @app.route('/tabory')
+def index ():
+    return render_template ("tabory.html")
+
+@app.route('/dobrovolnicti')
+def index ():
+    return render_template ("dobrovolnictvi.html")
+
+@app.route('/prace')
+def index ():
+    return render_template ("prace.html")
+
+@app.route('/onas')
+def index ():
+    return render_template ("onas.html")
+
+    @app.route('/registrace')
+def index ():
+    return render_template ("registrace.html")
+    
+    @app.route('/prihlaseni')
+def index ():
+    return render_template ("prihlaseni.html")
+
+
+
