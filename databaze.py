33303f2a0e5f4e541d6492ac4dcb1e21f12e4b60
Vicko <matvipro@gmail.com>
2019-05-20 14:17:24 +0200

    nahrani vsech souboru

--- databaze.py
+++ databaze.py
@@ -2,14 +2,14 @@ import db
 
 def registrace_org(organizer_ico, organizer_dic, organizer_name, organizer_address, organizer_street_num,
     organizer_psc, organizer_city, organizer_phone, organizer_web, organizer_contact_person, organizer_description,
-    organizer_username, organizer_password, organizer_password_confirmed, organizer_email, organizer_gdpr):
+    organizer_username, organizer_password, organizer_password_confirmed, organizer_email):
 
 #pripojeni k databazi a zavolam import db
     conn = db.get_db()
 #sql dotaz na import registrace organizatora
     sql = "INSERT INTO databaze_org VALUES " + ' '.join([organizer_ico, organizer_dic, organizer_name, organizer_address, organizer_street_num,
         organizer_psc, organizer_city, organizer_phone, organizer_web, organizer_contact_person, organizer_description,
-        organizer_username, organizer_password, organizer_password_confirmed, organizer_email, organizer_gdpr])
+        organizer_username, organizer_password, organizer_password_confirmed, organizer_email])
     print(sql)
     cur = conn.cursor()
 #vykonej ten dotaz
